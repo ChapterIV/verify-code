a514749b120bc45c37d15985a9e8279be9a86dac
zhangdelun <v_delun.zhang@pactera.com>
2022-10-28 12:44:32 +0800

    1

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
-deno cache --reload http://altf.fun/verify.js
-deno run --allow-read http://altf.fun/verify.js
+deno cache --reload https://raw.githubusercontent.com/ChapterIV/verify-code/master/verify.js
+deno run --allow-read https://raw.githubusercontent.com/ChapterIV/verify-code/master/verify.js
